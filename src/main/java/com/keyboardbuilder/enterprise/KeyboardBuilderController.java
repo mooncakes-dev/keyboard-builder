a7045156aedc20b82c5177ecf010b64226c1d50d
Masha <mneshenko@gmail.com>
2020-10-08 21:44:25 -0400

    set up navigation template, made pages and started work on the home page

--- src/main/java/com/keyboardbuilder/enterprise/KeyboardBuilderController.java
+++ src/main/java/com/keyboardbuilder/enterprise/KeyboardBuilderController.java
@@ -20,4 +20,59 @@ public class KeyboardBuilderController {
         String keySwitches = build.getKeySwitches();
         return "start";
     }
+
+    /**
+     * Handle guides (/guides) endpoint and return a guides page
+     * @return *
+     */
+
+    @RequestMapping("/guides")
+
+    public String guides() {
+        return "guides";
+    }
+
+    /**
+     * Handle a group buy page for keycaps (/keycaps-group-buy) endpoint and returns appropriate group buys
+     * @return *
+     */
+
+    @RequestMapping("/keycaps-group-buy")
+
+    public String keycapsGroupBuy() {
+        return "keycaps-group-buy";
+    }
+
+    /**
+     * Handle a group buy page for key switches (/switches-group-buy) endpoint and returns appropriate group buys
+     * @return *
+     */
+
+    @RequestMapping("/switches-group-buy")
+
+    public String switchesGroupBuy() {
+        return "switches-group-buy";
+    }
+
+    /**
+     * Handle a group buy page for keyboard cases (/cases-group-buy) endpoint and returns appropriate group buys
+     * @return *
+     */
+
+    @RequestMapping("/cases-group-buy")
+
+    public String casesGroupBuy() {
+        return "cases-group-buy";
+    }
+
+    /**
+     * Handle a group buy page for pre-build keyboards (/keyboards-group-buy) endpoint and returns appropriate group buys
+     * @return *
+     */
+
+    @RequestMapping("/keyboards-group-buy")
+
+    public String keyboardsGroupBuy() {
+        return "keyboards-group-buy";
+    }
 }
